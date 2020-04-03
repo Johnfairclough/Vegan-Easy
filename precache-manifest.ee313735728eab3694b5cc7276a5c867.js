805177eb8ec3b0924f9ad4e5707fe2bf70276b0d
John Fairclough <johnmfairclough@gmail.com>
2020-04-03 17:52:46 +0100

    Updates

---
+++ precache-manifest.ee313735728eab3694b5cc7276a5c867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac7b82834b4ce259e7afc54c1b30c36",
+    "url": "/vegan-easy-v.2/index.html"
+  },
+  {
+    "revision": "55e75524016d163f94e4",
+    "url": "/vegan-easy-v.2/static/css/main.1c2b8f17.chunk.css"
+  },
+  {
+    "revision": "26fb5468ff2c7fa6bff4",
+    "url": "/vegan-easy-v.2/static/js/2.2241fcb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vegan-easy-v.2/static/js/2.2241fcb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e75524016d163f94e4",
+    "url": "/vegan-easy-v.2/static/js/main.bb790ed8.chunk.js"
+  },
+  {
+    "revision": "420042fae5921782c65f",
+    "url": "/vegan-easy-v.2/static/js/runtime-main.26b2adbf.js"
+  }
+]);
